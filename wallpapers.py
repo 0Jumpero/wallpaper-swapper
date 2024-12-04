13e349133bb2ef3715491fe9eb366281cc6e22d0
0Jumpero <53096124+0Jumpero@users.noreply.github.com>
2024-12-04 16:41:00 +0100

    Update wallpapers.py
    Changes for standalone app

--- wallpapers.py
+++ wallpapers.py
@@ -2,20 +2,121 @@
 
 import ctypes
 import os
+from io import BytesIO
 import random
 from PIL import Image
 import requests
 import winreg
 import json
+import schedule
+import tkinter as tk
+import time
+import pystray
+from threading import Thread
 
-# Unsplash API key
-key = 'your-api-key'
+# Load settings
+try:
+  print("Loading settings...")
+  with open('settings.json', 'r', encoding='utf-8') as f:
+    settings = json.load(f)
+    key = settings['key']
+    interval = settings['interval']
+    lock_toggle = settings['lock_toggle']
+    query = settings['query']
+    pictures_folder = settings['pictures_folder']
+except FileNotFoundError:
+  print("Failed to load settings. Using default values...")
+  key = 'your-api-key'
+  interval = 30
+  lock_toggle = False
+  query = 'nature'
+  pictures_folder = os.path.join('C:\\', 'Users', 'Public', 'Pictures', 'Wallpapers')
 
 # Paths
-pictures_folder = os.path.join('C:\\', 'Users', 'Public', 'Pictures', 'Wallpapers')
 main_wallpaper_path = os.path.join(pictures_folder, "main_wallpaper.jpg")
 lock_screen_wallpaper_path = os.path.join(pictures_folder, "lock_screen_wallpaper.jpg")
 json_file_path = os.path.join(pictures_folder, 'wallpapers.json')
+
+# Check JSON
+def check_wallpapers_JSON(json_file_path):
+  print("Checking wallpapers database JSON...")
+  if os.path.exists(json_file_path):
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+      data = json.load(f)
+      if len(data) < 300:
+        return fetch_wallpapers_db(json_file_path)
+  else:
+    return fetch_wallpapers_db(json_file_path)
+  return 0
+
+# Get wallpapers from Unsplash
+def fetch_wallpapers_db(json_file_path):
+  global query
+  wallpapers = []
+  page = 0
+
+  # Check if api key is set
+  if key == 'your-api-key':
+    print("Error: API key not set. Set it in settings.json.")
+    root = tk.Tk()
+    root.title("API Error")
+    root.geometry("200x100")
+    root.resizable(False, False)
+    root.attributes('-toolwindow', True, '-topmost', True)
+    tk.Label(root, text = "Error: API key not set.\nSet it in settings.json").place(relx=0.5, rely=0.25, anchor="center")
+    tk.Button(root, text = "OK", width=10, command = root.destroy).place(relx=0.5, rely=0.7, anchor="center")
+    root.eval('tk::PlaceWindow . center')
+    root.mainloop()
+    return "API key not set."
+
+	# Download wallpapers infos from Unsplash
+  print("Fetching wallpapers database from Unsplash...")
+  while page < 50:
+    page += 1
+    url = f"https://api.unsplash.com/search/photos?query={query}&orientation=landscape&per_page=30&page={page}&client_id={key}"
+    try:
+      response = requests.get(url)
+      response.raise_for_status()  # Raise an error for bad status codes
+      data = response.json()
+      if 'results' in data:
+        wallpapers.extend(data['results'])
+      else:
+        print(f"Unexpected API response structure on page {page}.")
+        continue
+      print(f"Fetched page {page}, total wallpapers: {len(wallpapers)}")
+
+      # Check if we need to fetch more pages
+      if len(data['results']) == 0:
+        break
+
+    except requests.exceptions.RequestException as error:
+      print(f"Error fetching page {page}: {error}")
+      if page == 1: break
+      else: continue
+    except KeyError as e:
+      print(f"Error parsing API response: Missing key {e}")
+      break
+
+  # Check for errors
+  if len(wallpapers) == 0:
+    print("Error fetching wallpapers from Unsplash.")
+    return "Error fetching wallpapers from Unsplash."
+
+  # Check if wallpapers folder exists
+  if not os.path.exists(json_file_path):
+    print(f"Creating wallpapers folder at {os.path.dirname(json_file_path)}...")
+    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+  
+  # Save wallpapers infos to JSON file
+  try:
+    with open(json_file_path, 'w') as file:
+      json.dump(wallpapers, file, indent=2)
+    print(f"Saved {len(wallpapers)} wallpapers to {json_file_path}")
+  except Exception as e:
+    print(f"Error saving wallpapers to {json_file_path}: {e}")
+    return "Error saving wallpapers to JSON file."
+
+  return 0
 
 # Load JSON and get random URLs
 def pick_random_wallpapers(json_file_path):
@@ -82,10 +183,9 @@ def download_wallpapers(urls, folder):
     print(f"Wallpaper {file_name} saved to {file_path}")
   return wallpapers
 
-
 # Set the main wallpaper
 def set_desktop_wallpaper(image_path):
-  if(image_path == 0): return 1
+  if(not image_path): return "Error: Invalid desktop image path."
   print('Setting desktop wallpaper...')
   try:
     key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Desktop", 0, winreg.KEY_SET_VALUE)
@@ -98,11 +198,11 @@ def set_desktop_wallpaper(image_path):
     return 0
   except Exception as e:
     print(f"Error setting desktop wallpaper: {e}")
-    return 1
+    return "Error setting desktop wallpaper."
 
 # Set the lock screen wallpaper
 def set_lock_screen_wallpaper(image_path):
-  if(image_path == 0): return 1
+  if(image_path == 0): return "Error: Invalid lock image path."
   print('Setting lockscreen wallpaper...')
   try:
     key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Policies\Microsoft\Windows\Personalization", 0, winreg.KEY_SET_VALUE)
@@ -117,80 +217,9 @@ def set_lock_screen_wallpaper(image_path):
     return 0
   except Exception as e:
     print(f"Error setting lockscreen wallpaper: {e}")
-    return 1
+    return "Error setting lockscreen wallpaper."
 
-# Get wallpapers from Unsplash
-def fetch_wallpapers_db(json_file_path):
-  query = 'nature' # Change the query to selected wallpaper topic
-  wallpapers = []
-  page = 0
-
-  # Check if api key is set
-  if key == 'your-api-key':
-    print("Error: API key not set.")
-    return 1
-
-	# Download wallpapers infos from Unsplash
-  print("Fetching wallpapers database from Unsplash...")
-  while page < 50:
-    page += 1
-    url = f"https://api.unsplash.com/search/photos?query={query}&orientation=landscape&per_page=30&page={page}&client_id={key}"
-    try:
-      response = requests.get(url)
-      response.raise_for_status()  # Raise an error for bad status codes
-      data = response.json()
-      if 'results' in data:
-        wallpapers.extend(data['results'])
-      else:
-        print(f"Unexpected API response structure on page {page}.")
-        continue
-      print(f"Fetched page {page}, total wallpapers: {len(wallpapers)}")
-
-      # Check if we need to fetch more pages
-      if len(data['results']) == 0:
-        break
-
-    except requests.exceptions.RequestException as error:
-      print(f"Error fetching page {page}: {error}")
-      continue
-    except KeyError as e:
-      print(f"Error parsing API response: Missing key {e}")
-      continue
-
-  # Check for errors
-  if len(wallpapers) == 0:
-    print("Error fetching wallpapers from Unsplash.")
-    return 1
-
-  # Check if wallpapers folder exists
-  if not os.path.exists(json_file_path):
-    print(f"Creating wallpapers folder at {os.path.dirname(json_file_path)}...")
-    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
-  
-  # Save wallpapers infos to JSON file
-  try:
-    with open(json_file_path, 'w') as file:
-      json.dump(wallpapers, file, indent=2)
-    print(f"Saved {len(wallpapers)} wallpapers to {json_file_path}")
-  except Exception as e:
-    print(f"Error saving wallpapers to {json_file_path}: {e}")
-    return 1
-
-  return 0
-
-# Check JSON
-def check_wallpapers_JSON(json_file_path):
-  print("Checking wallpapers database JSON...")
-  if os.path.exists(json_file_path):
-    with open(json_file_path, 'r', encoding='utf-8') as f:
-      data = json.load(f)
-      if len(data) < 300:
-        return fetch_wallpapers_db(json_file_path)
-  else:
-    return fetch_wallpapers_db(json_file_path)
-  return 0
-
-# Main function
+# Main function to change wallpapers
 def change_wallpaper():
 
   # Check database
@@ -198,20 +227,153 @@ def change_wallpaper():
 
   # Get random wallpapers from JSON file
   if status == 0: urls = pick_random_wallpapers(json_file_path)
-  else: urls = []
 
   # Download wallpapers
-  if urls != []: wallpaper_files = download_wallpapers(urls, pictures_folder)
-  else: wallpaper_files = [0,0]
+  if status == 0: wallpaper_files = download_wallpapers(urls, pictures_folder)
 
   # Set the first wallpaper as desktop background
-  status = set_desktop_wallpaper(wallpaper_files[0])
+  if status == 0: status = set_desktop_wallpaper(wallpaper_files[0])
 
   #Set the second wallpaper as lock screen background
-  status = status + set_lock_screen_wallpaper(wallpaper_files[1])
+  if lock_toggle and status == 0: status = set_lock_screen_wallpaper(wallpaper_files[1])
 
   if status == 0: print("Wallpapers changed successfully!")
-  else: print("Error changing wallpapers.")
+  else: print(f'Error changing wallpapers. Error message: "{status}"')
 
-# Run the main function
-change_wallpaper()
+  return status
+
+# Save settings
+def save_settings():
+  try:
+    with open('settings.json', 'w', encoding='utf-8') as f:
+      global key, pictures_folder, interval, lock_toggle, query
+      json.dump({
+        'key': key, 
+        'interval': interval, 
+        'lock_toggle': lock_toggle,
+        'query': query,
+        'pictures_folder': pictures_folder 
+      }, f, indent=2)
+    print("Settings saved.")
+  except Exception as e:
+    print(f"Error saving settings: {e}")
+
+# Set interval function
+def set_interval(new_interval):
+  global interval, run, scheduler
+  interval = new_interval
+  print("Quitting current schedule...")
+  run = False
+  scheduler.join()
+  print("Schedule quit.\nStarting new schedule...")
+  run = True
+  scheduler = Thread(target=run_schedule, daemon=True)
+  scheduler.start()
+  print(f"Schedule with {new_interval}min interval started.")
+  save_settings()
+
+# Elevate to admin 
+def elevate():
+  print("Relaunching as admin...")
+  ctypes.windll.shell32.ShellExecuteW(None, "runas", "pythonw", __file__, None, 1)
+  os._exit(0)
+
+# Toggle lockscreen function
+def toggle_lockscreen(setting):
+  global lock_toggle
+  if setting == True:
+    print("Checking admin privileges...")
+    try:
+      isAdmin = ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+      isAdmin = False
+
+    if isAdmin:
+      lock_toggle = True
+      print(f"Lockscreen toggle set to {lock_toggle}.")
+      save_settings()
+    else:
+      print("Lacking admin privileges.")
+      root = tk.Tk()
+      root.geometry("300x120")
+      root.resizable(False, False)
+      root.title("Admin mode required")
+      root.attributes('-toolwindow', True, '-topmost', True)
+      tk.Label(root, text="To change the lockscreen wallpaper you need to \nrelaunch the program in admin mode. Continue?", padx=10, pady=10).place(relx=0.5, rely=0.3, anchor="center")
+      tk.Button(root, text="Yes", width=10, command=elevate).place(relx=0.3, rely=0.7, anchor="center")
+      tk.Button(root, text="No", width=10, command=lambda: root.destroy()).place(relx=0.7, rely=0.7, anchor="center")
+      root.eval('tk::PlaceWindow . center')
+      root.mainloop()
+  else:
+    lock_toggle = False
+    print(f"Lockscreen toggle set to {lock_toggle}.")
+    save_settings()
+
+# Tray menu setup
+def tray_setup():
+  print("Setting up tray menu...\nDownloading icon...")
+  img_url = "https://cdn-icons-png.flaticon.com/512/1046/1046493.png"
+  try:
+    response = requests.get(img_url, stream=True)
+    response.raise_for_status()
+  except requests.exceptions.RequestException as e:
+    print(f"Error downloading icon: {e}")
+    return
+  
+  print("Converting menu items...")
+  img = Image.open(BytesIO(response.content))
+  tray = pystray.Icon("wallpaper-changer", img, "Wallpaper Changer", menu=(
+    pystray.MenuItem("Change wallpaper", lambda: change_wallpaper()),
+    pystray.MenuItem("Toggle Lockscreen", lambda: toggle_lockscreen(not lock_toggle), checked=lambda item: lock_toggle),
+    pystray.MenuItem("Set Interval", pystray.Menu(
+      pystray.MenuItem("15 minutes", lambda: set_interval(15), checked=lambda item: interval == 15),
+      pystray.MenuItem("30 minutes", lambda: set_interval(30), checked=lambda item: interval == 30),
+      pystray.MenuItem("1 hour", lambda: set_interval(60), checked=lambda item: interval == 60),
+      pystray.MenuItem("2 hours", lambda: set_interval(120), checked=lambda item: interval == 120),
+    )),
+    pystray.MenuItem("Quit", lambda: tray.stop()),
+  ))
+  print("Starting tray icon...")
+  tray.run()
+
+# Schedule thread
+def run_schedule():
+  global run, interval
+
+  print("Starting schedule thread...")
+  # Schedule the wallpaper change
+  schedule.every(interval).minutes.do(change_wallpaper)
+
+  # Run the schedule
+  while run:
+    schedule.run_pending() 
+    time.sleep(1)
+
+  # Stop the schedule
+  schedule.clear()
+  print("Terminating schedule thread...")
+
+# Main thread globals
+run = True
+scheduler = None
+
+# Main thread
+if __name__ == "__main__": 
+  # Check lockscreen toggle requirement
+  if lock_toggle: toggle_lockscreen(lock_toggle)
+
+  # Initial wallpaper change
+  status = change_wallpaper()
+
+  if status == 0: 
+    # Start the schedule in a separate thread
+    scheduler = Thread(target=run_schedule, daemon=True)
+    scheduler.start()
+
+    # Show the tray icon
+    tray_setup()
+
+    # Stop the schedule when the tray icon is closed
+    print("Quitting...")
+    run = False
+    scheduler.join()
